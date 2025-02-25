4f27cd32a3e4a50c54bf90ddac2a588147f1b477
xNakiri24 <christianalupit24@outlook.com>
2025-02-25 08:26:35 +0800

    Palindrome Checking is working and added  definition checking using Urban Dictionary because of cost to be cover if I use AI. Will change later to AI definition checker later

--- script/script.js
+++ script/script.js
@@ -5,28 +5,56 @@ form.addEventListener('submit', async (event) => {
 
     const word = document.getElementById("myWord").value;
     const li = document.createElement("li");
+    const p = document.createElement("p")
+    const details = document.createElement("details");
+    const summary = document.createElement("summary");
 
-    // const url = 'https://urban-dictionary7.p.rapidapi.com/v0/define?term=yeet';
-    // const options = {
-    //     method: 'GET',
-    //     headers: {
-    //         'x-rapidapi-key': 'Sign Up for Key',
-    //         'x-rapidapi-host': 'urban-dictionary7.p.rapidapi.com'
-    //     }
-    // };
+    function isPalindrome(word) {
+        return word === word.split('').reverse().join('');
+    }
+    console.log(isPalindrome(word)); 
 
-    // try {
-    //     const response = await fetch(url, options);
-    //     const result = await response.text();
-    //     console.log(result);
-    // } catch (error) {
-    //     console.error(error);
-    // }
+    let resultWord = ""
+    if(!isPalindrome(word)){
+        resultWord = "Not a palindrome"
+    }else{
+        resultWord = "Word is a palindrome"
+    }
 
-    localStorage.setItem("analyzedWord", word);
-    li.innerHTML = localStorage.getItem("analyzedWord");
-    li.classList.add("list-word")
-    document.getElementById("myList").appendChild(li);
+    const url = `https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${word}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '5e863294damshf24c1ceb1dba2e9p17223djsn9efabe82a2b4',
+            'x-rapidapi-host': 'mashape-community-urban-dictionary.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+
+        if (result.list && result.list.length > 0) {
+            const firstDefinition = result.list[0].definition;
+            console.log("First Definition:", firstDefinition);
+
+            summary.innerHTML = localStorage.getItem("analyzedWord") + " - " + resultWord;
+            p.innerHTML = firstDefinition;
+
+            details.appendChild(summary);
+            details.appendChild(p);
+
+            localStorage.setItem("analyzedWord", word);
+            li.appendChild(details);
+            li.classList.add("list-word")
+            document.getElementById("myList").appendChild(li);
+        } else {
+            console.log("No definitions found.");
+            
+        }
+    } catch (error) {
+        console.error(error);
+    }
 
     form.reset();
 });
